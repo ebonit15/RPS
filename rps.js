4590fffd4321d306f51231a670a2ccb43fce74b8
ebonit15 <ebonit15@gmail.com>
2022-11-03 21:15:21 +0300

    Carried playerSelection inside of the playRound function. Placed a score counter, but couldn't manage to make the loop work properly for a 5 round game.

--- rps.js
+++ rps.js
@@ -3,10 +3,9 @@ let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
 
+//randomly generating a move for the computer//
 function getComputerSelection() {
     
-    //randomly generating a move for the computer//
-
     let a = Math.floor(Math.random() * 30)
     console.log(a);
     
@@ -19,32 +18,65 @@ function getComputerSelection() {
     else return(scissors);
 }
 
-let computerSelection = getComputerSelection();
+let counter;
 
-console.log(computerSelection);
+//Function to call out the result of matches.//
+function playRound () {
 
-function playRound (playerSelection) {
+    //Asking for player's selection//
+    let playerSelection = (prompt("Rock, paper, scissors?")).toLowerCase();
+    console.log(playerSelection);
+
+    let computerSelection = getComputerSelection();
+    console.log(computerSelection);
+
+
     if ((computerSelection === "paper") && (playerSelection === "rock")) {
+        counter = 0;
         return("You lose, paper beats rock!");
     }
     else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
+        counter = 0;
         return("You lose, rock beats scissors!");
     }
 
     else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
+        counter = 0;
         return("You lose, scissors beats paper!");
     }
     
     else if ((computerSelection === "scissors") && (playerSelection === "rock")) {
+        counter = 1;
         return("You won, rock beats scissors!");
     }
     
     else if ((computerSelection === "paper") && (playerSelection === "scissors")) {
+        counter = 1;
         return("You won, scissors beats paper!");
     }
     else if ((computerSelection === "rock") && (playerSelection === "paper")) {
+        counter = 1;
         return("You won, paper beats rock!");
     }
     else return("No winner!")
 
+}
+
+//Function to count score//
+function score () {
+    let plyr=0;
+    let cmptr=0;
+    if (counter > 0) {
+        plyr++;
+    }
+    
+    else cmptr++;
+    return("You:"+plyr+" "+"Computer:"+cmptr);
+}
+
+function game() {
+    for (i=0; i<5; i++) {
+        playRound();
+        score();
+    }
 }
